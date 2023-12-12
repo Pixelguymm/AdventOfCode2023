04970b6306c66fe11c11c88d41c248bae0da4af2
Pixelguymm <maartenmarx@hotmail.be>
2023-12-12 16:51:59 +0100

    Measure runtime

--- src/main/kotlin/Day.kt
+++ src/main/kotlin/Day.kt
@@ -1,10 +1,16 @@
+import kotlin.system.measureTimeMillis
+
 abstract class Day {
     abstract fun part1(): Any
 
     abstract fun part2(): Any
 
     fun showResults() {
-        println(part1())
-        println(part2())
+        measureTimeMillis {
+            println(part1())
+        }.also { println("Time in ms: $it") }
+        measureTimeMillis {
+            println(part2())
+        }.also { println("Time in ms: $it") }
     }
 }
